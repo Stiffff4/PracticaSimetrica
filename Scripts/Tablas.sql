e6b0a0e0bff129d4fa250eca36e2142bf0b2b400
Stiffff4 <61694466+Stiffff4@users.noreply.github.com>
2021-06-16 12:35:05 -0400

    Actualizando el script

--- Scripts/Tablas.sql
+++ Scripts/Tablas.sql
@@ -1,1 +1,18 @@
+create table evaluacion (
+	id int primary key identity,
+	fecha_evaluacion date not null,
+	resultado_examen_psicologico varchar(1) not null,
+	resultado_examen_algoritmos varchar(1) not null,
+	resultado_examen_sql varchar(1) not null,
+	resultado_final varchar(1) not null
+)
 
+create table candidato (
+	id int primary key identity,
+	id_evaluacion int not null references evaluacion(id),
+	nombre varchar(30) not null,
+	apellido varchar(30) not null,
+	edad int not null,
+	fecha_nacimiento Datetime not null,
+	cedula varchar(13) unique not null
+)
